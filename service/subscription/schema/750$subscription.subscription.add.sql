239921efcd535cdc8f702cf80fde34db78ff15e2
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-04-21 18:51:54 +0300

    fix: add phone.add procedure and refactor subscription.add/fetch

--- service/subscription/schema/750$subscription.subscription.add.sql
+++ service/subscription/schema/750$subscription.subscription.add.sql
@@ -8,24 +8,28 @@
   "isSingleResult" boolean
 )
 AS
-$body$
-  WITH
-  p as (
-    INSERT INTO subscription.phone ("phoneNumber")
-    VALUES ("@phoneNumber")
-    RETURNING *
-  ),
-  s as (
-    INSERT INTO subscription.subscription ("phoneId", "actorId")
-    VALUES ((SELECT "phoneId" FROM p), "@actorId")
-    RETURNING *
-  )
-
+$BODY$
+DECLARE
+	"@phoneId" INTEGER := (SELECT ph."phoneId" FROM subscription."phone.add"("@phoneNumber") ph);
+	"@subscriptionId" INTEGER;
+BEGIN
+ WITH
+   s as (
+      INSERT INTO subscription.subscription ("phoneId", "actorId")
+      VALUES ("@phoneId", "@actorId")
+      RETURNING *
+    )
+    SELECT
+      s."subscriptionId"
+    INTO
+      "@subscriptionId"
+    FROM s;
+RETURN QUERY
   SELECT
-    s."subscriptionId",
-    s."actorId",
-    p."phoneNumber",
-    true AS "isSingleResult"
-  FROM s, p
-$body$
-LANGUAGE SQL
+    "@subscriptionId" as "subscriptionId",
+    "@actorId" as "actorId",
+    "@phoneNumber" as "phoneNumber",
+    true AS "isSingleResult";
+END
+$BODY$
+LANGUAGE plpgsql;
