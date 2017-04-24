a122322dd8228615da87b325f5d331ca18cd307b
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-04-24 19:53:00 +0300

    fix: add subscription.fetch as api call

--- service/subscription/api.js
+++ service/subscription/api.js
@@ -8,6 +8,14 @@ module.exports = {
     params: joi.any(),
     result: joi.any()
   },
+  'subscription.fetch': {
+    // tags: ['tag1', 'tag2'],
+    description: '',
+    notes: '',
+    auth: false,
+    params: joi.any(),
+    result: joi.any()
+  },
   'subscription.add': {
     // tags: ['tag1', 'tag2'],
     description: '',
