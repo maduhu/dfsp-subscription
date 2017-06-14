94cf4d192f98f2153d31f23ccf3bff566ec207f7
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-06-14 16:42:37 +0300

    fix: add init integration tests

---
+++ test/integration/test.subscription.add.js
@@ -0,0 +1,54 @@
+var test = require('ut-run/test')
+var config = require('./../lib/appConfig')
+
+const PHONE_NUMBER = '08994746666'
+const ACTOR_ID = '1'
+
+test({
+  type: 'integration',
+  name: 'Subscription',
+  server: config.server,
+  serverConfig: config.serverConfig,
+  client: config.client,
+  clientConfig: config.clientConfig,
+  steps: function (test, bus, run) {
+    return run(test, bus, [{
+      name: 'Missing input params - Raise exception for missing actorr id',
+      method: 'subscription.subscription.add',
+      params: (context) => {
+        return {}
+      },
+      error: (error, assert) => {
+        assert.equal(error.errorPrint, 'subscription.actorIdMissing', error.errorPrint)
+      }
+    },
+    {
+      name: 'Missing input params - Raise exception for missing phone number',
+      method: 'subscription.subscription.add',
+      params: (context) => {
+        return {
+          actorId: ACTOR_ID
+        }
+      },
+      error: (error, assert) => {
+        assert.equal(error.errorPrint, 'subscription.phoneNumberMissing', error.errorPrint)
+      }
+    },
+    {
+      name: 'Missing input params - Raise exception for missing phone number',
+      method: 'subscription.subscription.add',
+      params: (context) => {
+        return {
+          actorId: ACTOR_ID,
+          phoneNumber: PHONE_NUMBER
+        }
+      },
+      result: (result, assert) => {
+        assert.equal(result.actorId, ACTOR_ID, 'Check actorId result')
+        assert.equal(result.phoneNumber, PHONE_NUMBER, 'Check phoneNumber result')
+        assert.true((typeof result.subscriptionId === 'number'), 'Check subscriptionId result type')
+      }
+    }
+    ])
+  }
+}, module.parent)
