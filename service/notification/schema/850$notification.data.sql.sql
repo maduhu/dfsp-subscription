b35a05caafa4fc3fa4d9971be3064c336ea86831
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-06-22 11:19:32 +0300

    fix: add more notification operations

--- service/notification/schema/850$notification.data.sql.sql
+++ service/notification/schema/850$notification.data.sql.sql
@@ -32,7 +32,15 @@ ON CONFLICT ("notificationChannelId") DO UPDATE SET
 INSERT INTO
   notification."notificationOperation" ("notificationOperationId", "name", "description", "params")
 VALUES
-  (1, 'p2p', 'desc for test p2p operation!', '{"amount", "currency"}')
+  (1, 'p2p', 'desc for test p2p operation!', '{"transferId", "receiver", "sourceAccount", "destinationAmount", "sourceIdentifier"}'),
+  (2, 'invoice', 'desc for test invoce operation!', '{"amount", "currency"}'),
+  (3, 'cashIn', 'desc for test cashIn operation!', '{"amount", "currency"}'),
+  (4, 'cashOut', 'desc for test cashOut operation!', '{"amount", "currency"}'),
+  (5, 'bulkPayment', 'desc for test bulkPayment operation!', '{"amount", "currency"}'),
+  (6, 'invoiceCancel', 'desc for test invoiceCancel operation!', '{"invoiceNotificationId", "invoiceUrl", "identifier", "status", "memo"}'),
+  (7, 'addHolder', 'desc for test addHolder operation!', '{"actorId", "accountNumber", "isDefault", "isSignatory", "identifier"}'),
+  (8, 'removeHolder', 'desc for test removeHolder operation!', '{"actorAccountId"}'),
+  (9, 'walletAdd', 'desc for test walletAdd operation!', '{"actor"}')
 ON CONFLICT ("notificationOperationId") DO UPDATE SET
   "name" = EXCLUDED."name",
   "description" = EXCLUDED."description",
